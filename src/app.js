518c73c889d9067aa7d8f60238cf5ada0bce2ac5
zeeshan <zeeshan2211997@gmail.com>
2024-11-12 16:01:17 +0530

    APIs and Validation

--- src/app.js
+++ src/app.js
@@ -11,10 +11,68 @@ app.post("/signup", async (req, res) => {
     try {
         await user.save();
         res.send("User added!")
+    } catch (err) {
+        res.status(400).send("err:" + err.message)
+    }
+})
+
+app.get("/user", async (req, res) => {
+    const userMail = req.body.emaiId
+    try {
+        const user = await User.find({ emaiId: userMail });
+        if (user.length === 0) {
+            res.status(404).send("User not found")
+        }
+        else {
+            res.send(user);
+        }
     } catch {
         res.status(400).send("err:" + err.message)
     }
 })
+
+app.get("/feed", async (req, res) => {
+    try {
+        const user = await User.find({});
+        res.send(user);
+    } catch {
+        res.status(400).send("err:" + err.message)
+    }
+})
+
+app.delete("/deleteUser", async (req, res) => {
+    const userId = req.body.userId
+    try {
+        const user = await User.findByIdAndDelete(userId)
+        res.send("DELETED")
+    }
+    catch (err) {
+        res.status(400).send("err:" + err.message)
+    }
+})
+
+
+app.patch("/updateUser/:userId", async (req, res) => {
+    const userId = req.params?.userId;
+    const data = req.body
+
+    try {
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+        const user = await User.findByIdAndUpdate(userId, data, {
+            runValidators: true
+        })
+        res.send("user data updated succefully!!!")
+    }
+    catch (err) {
+        res.status(400).send("err:" + err.message)
+    }
+})
+
+
 
 
 connectDB().then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,58 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
-    firstName:{
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
         type: String
     },
-    lastName:{
-        type: String
+    emailId: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Not a valid EmailId Address");
+            }
+        }
     },
-    emaiId:{
-        type: String
+    password: {
+        type: String,
+        required: true
     },
-    password:{
-        type: String
+    age: {
+        type: Number,
+        min: 18
     },
-    age:{
-        type: Number
+    gender: {
+        type: String,
+        validate(value) {
+            if (!["Male", "Female"].includes(value)) {
+                throw new Error("Not a valid Gender");
+            }
+        }
     },
-    gender:{
-        type: String
+    photoUrl: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg"
+    },
+    about: {
+        type: String,
+        default: "This is default"
+    },
+    skills: {
+        type: [String]
     }
-})
+},
+    {
+        timestamps: true
+    }
+)
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
